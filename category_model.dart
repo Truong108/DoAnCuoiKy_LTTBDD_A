6709ddd2b07aa93d387d4220d75d46c6dc6a9cb2
Xuan Truong <121649515+Truong108@users.noreply.github.com>
2023-05-22 20:40:35 +0700

    Add files via upload

---
+++ category_model.dart
@@ -0,0 +1,14 @@
+class Category {
+  final int id;
+  final String name;
+
+  Category(this.id, this.name);
+}
+
+List<Category> categories = [
+  Category(0, 'All'),
+  Category(1, 'Cá'),
+  Category(2, 'Gà'),
+  Category(3, 'Tôm'),
+  Category(4, 'Nước'),
+];
