e7232d4150191b566f6eecde31afda2227fd30fe
Xuan Truong <121649515+Truong108@users.noreply.github.com>
2023-05-22 20:40:02 +0700

    Add files via upload

---
+++ Colors.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color primaryColor = Colors.green;
+  static const Color borderColor = Colors.grey;
+  static const Color errorColor = Colors.red;
+
+}
